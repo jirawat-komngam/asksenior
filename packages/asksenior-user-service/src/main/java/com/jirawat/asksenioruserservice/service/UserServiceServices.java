bae9db71940f1c4605613ef5ac288e1705a844d1
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-10 16:04:59 +0700

    feat: createOTP-service

--- packages/asksenior-user-service/src/main/java/com/jirawat/asksenioruserservice/service/UserServiceServices.java
+++ packages/asksenior-user-service/src/main/java/com/jirawat/asksenioruserservice/service/UserServiceServices.java
@@ -1,5 +1,7 @@
 package com.jirawat.asksenioruserservice.service;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +32,24 @@ public class UserServiceServices {
     }
 
     public void createOTP(String userEmail) {
+        log.info("recieve email %s", userEmail, " and ready for create otp");
+        String numbers = "0123456789";
+        Random rndm_method = new Random();
+        char[] otp = new char[6];
+        for (int i = 0; i < 6; i++) {
+            otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));
+        }
+        log.info("create otp %s%s", String.valueOf(otp), " complete by user email is ", userEmail);
+        String strotp = String.valueOf(otp);
+        String Email = userEmail;
+        log.info("send otp :%s%s", String.valueOf(otp), " and email : ", userEmail, "to redis");
+        sendEmail(userEmail);
+        log.info("send otp :%s%s", String.valueOf(otp), " to email : ", userEmail, "success");
 
+    }
+
+    public void sendEmail(String userEmail) {
+        log.info("send email success");
     }
 
 }
