a08d9be763c9378f728f56abe47352bb0b4bbfc7
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-12 08:36:33 +0700

    feature: example controller

--- packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostControllers/PostController.java
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostControllers/PostController.java
@@ -3,8 +3,14 @@ package com.jirawat.askseniorpostservice.PostControllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jirawat.askseniorpostservice.DTOs.ResponseDTO;
+
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,9 +19,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping(path = "/api/v1/posts")
 public class PostController {
     @GetMapping(path = "/user/{userEmail}")
-    public String getPostByUserID(@PathVariable("userEmail") String userEmail) {
+    public ResponseEntity<ResponseDTO<String, String>> getPostByUserID(@PathVariable("userEmail") String userEmail) {
         log.info("get postByUserID controller with userEmail : {}", userEmail);
-        return "test " + userEmail;
+        return new ResponseEntity<>(new ResponseDTO<>("OK", null), HttpStatus.OK);
     }
 
 }
