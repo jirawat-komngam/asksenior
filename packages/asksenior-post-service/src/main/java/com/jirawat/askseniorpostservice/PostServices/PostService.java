11acda4f146c81c91f2e7e39b95af18abe1e2b86
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-12 13:09:10 +0700

    feature: initial getpostbyPostID

---
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostServices/PostService.java
@@ -0,0 +1,13 @@
+package com.jirawat.askseniorpostservice.PostServices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jirawat.askseniorpostservice.Entities.Post;
+
+public class PostService {
+    public List<Post> getPostByPostID(String postID) {
+        List<Post> newPostList = new ArrayList<>(null);
+        return newPostList;
+    }
+}
