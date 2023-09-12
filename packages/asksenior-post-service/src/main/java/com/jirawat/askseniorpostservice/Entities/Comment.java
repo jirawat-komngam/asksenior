a8df5ac66dda18db2cd12bf2d96ca4381e34d963
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-12 10:15:14 +0700

    feature: comment object

---
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/Entities/Comment.java
@@ -0,0 +1,18 @@
+package com.jirawat.askseniorpostservice.Entities;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Comment {
+    private UUID commentID;
+    private Integer userYear;
+    private String userID;
+    private String field;
+    private String commentContent;
+}
