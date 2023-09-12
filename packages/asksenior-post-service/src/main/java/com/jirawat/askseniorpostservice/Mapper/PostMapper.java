d2df9f1c8448a9e3e389bdee2aaa0f6a8c3d74ca
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-12 19:55:20 +0700

    feature: add post mapper

---
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/Mapper/PostMapper.java
@@ -0,0 +1,17 @@
+package com.jirawat.askseniorpostservice.Mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.jirawat.askseniorpostservice.DTOs.PostDTO;
+import com.jirawat.askseniorpostservice.Entities.Post;
+
+@Mapper
+public interface PostMapper {
+
+    PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
+
+    @Mapping(target = "faculties", expression = "java(null)")
+    PostDTO universityToUniversityDTO(Post post);
+}
