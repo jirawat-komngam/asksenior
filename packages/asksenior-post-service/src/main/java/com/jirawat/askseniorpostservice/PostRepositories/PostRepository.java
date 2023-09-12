8e8ece80c59957004ddd6f5fa3e7a10de1f669e3
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-12 16:12:48 +0700

    feature: repository findByUserID

--- packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostRepositories/PostRepository.java
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostRepositories/PostRepository.java
@@ -1,5 +1,14 @@
 package com.jirawat.askseniorpostservice.PostRepositories;
 
-public interface PostRepository {
+import java.util.UUID;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.jirawat.askseniorpostservice.Entities.Post;
+import java.util.List;
+
+public interface PostRepository extends CrudRepository<Post, UUID> {
+    Post findByFieldID(String fieldID);
+
+    List<Post> findByUserID(String userID);
 }
