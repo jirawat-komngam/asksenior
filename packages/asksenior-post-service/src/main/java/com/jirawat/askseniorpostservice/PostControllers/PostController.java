60211aefd7b0bb81623b77e0167f9026b280dc89
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-12 13:23:00 +0700

    feature: integrate controller to service

--- packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostControllers/PostController.java
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostControllers/PostController.java
@@ -1,29 +1,33 @@
 package com.jirawat.askseniorpostservice.PostControllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.jirawat.askseniorpostservice.DTOs.ResponseDTO;
-import com.jirawat.askseniorpostservice.Entities.Comment;
-import com.jirawat.askseniorpostservice.Entities.Post;
-
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jirawat.askseniorpostservice.DTOs.ResponseDTO;
+import com.jirawat.askseniorpostservice.Entities.Comment;
+import com.jirawat.askseniorpostservice.Entities.Post;
+import com.jirawat.askseniorpostservice.PostServices.PostService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequestMapping(path = "/api/v1/posts")
 public class PostController {
+    @Autowired
+    private PostService postService;
+
     @GetMapping(path = "/user/{userID}")
     public ResponseEntity<ResponseDTO<List<Post>, String>> getPostByUserID(@PathVariable("userID") String userID) {
         log.info("get postByUserID controller with userID : {}", userID);
@@ -43,15 +47,7 @@ public class PostController {
     @GetMapping(path = "/{postID}")
     public ResponseEntity<ResponseDTO<List<Post>, String>> getPostByPostID(@PathVariable("postID") UUID postID) {
         log.info("get postByPostID controller with userID : {}", postID);
-        List<Post> newPostListExample = new ArrayList<>();
-        Post newPostExample = new Post();
-        newPostExample.setComment("asdasdasdweee");
-        newPostExample.setFieldID("wdwdwrrrrwewdsds");
-        newPostExample.setPostDiscription("wewedefeefe");
-        newPostExample.setPostID(postID);
-        newPostExample.setPostTitle("wdwdwrrrr");
-        newPostExample.setUserID("dedesdw");
-        newPostListExample.add(newPostExample);
+        List<Post> newPostListExample = postService.getPostByPostID(postID);
         return new ResponseEntity<>(new ResponseDTO<>(newPostListExample, null), HttpStatus.OK);
     }
 
--- packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostServices/PostService.java
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostServices/PostService.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
 import com.jirawat.askseniorpostservice.Entities.Post;
 
+@Service
 public class PostService {
     public List<Post> getPostByPostID(UUID postID) {
         List<Post> newPostListExample = new ArrayList<>();
