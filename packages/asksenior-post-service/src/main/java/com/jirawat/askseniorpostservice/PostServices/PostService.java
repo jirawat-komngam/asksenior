3747746e25a9bc89d87950ea6f08110093be97dd
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-12 13:11:13 +0700

    feature: add mock detail into service

--- packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostServices/PostService.java
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostServices/PostService.java
@@ -2,12 +2,21 @@ package com.jirawat.askseniorpostservice.PostServices;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import com.jirawat.askseniorpostservice.Entities.Post;
 
 public class PostService {
-    public List<Post> getPostByPostID(String postID) {
-        List<Post> newPostList = new ArrayList<>(null);
-        return newPostList;
+    public List<Post> getPostByPostID(UUID postID) {
+        List<Post> newPostListExample = new ArrayList<>();
+        Post newPostExample = new Post();
+        newPostExample.setComment("asdasdasdweee");
+        newPostExample.setFieldID("wdwdwrrrrwewdsds");
+        newPostExample.setPostDiscription("wewedefeefe");
+        newPostExample.setPostID(postID);
+        newPostExample.setPostTitle("wdwdwrrrr");
+        newPostExample.setUserID("dedesdw");
+        newPostListExample.add(newPostExample);
+        return newPostListExample;
     }
 }
