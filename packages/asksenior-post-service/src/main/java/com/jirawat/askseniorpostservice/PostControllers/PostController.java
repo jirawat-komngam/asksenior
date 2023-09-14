e15240ec1449cf5fba6099a967a8fd0a95e10e02
jirawat-komngam <jirawat.komn@gmail.com>
2023-09-15 00:08:58 +0700

    refactor: cleaning organizing and logging code in post service

--- packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostControllers/PostController.java
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostControllers/PostController.java
@@ -30,40 +30,52 @@ public class PostController {
 
     @GetMapping(path = "/user/{userID}")
     public ResponseEntity<ResponseDTO<List<PostDTO>, String>> getPostByUserID(@PathVariable("userID") String userID) {
-        log.info("get postByUserID controller with userID : {}", userID);
+        log.info("get postByUserID controller with userID: {}", userID);
+
         List<PostDTO> getPostByUserID = postService.getPostByUserID(userID);
         return new ResponseEntity<>(new ResponseDTO<>(getPostByUserID, null), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{postID}")
     public ResponseEntity<ResponseDTO<PostDTO, String>> getPostByPostID(@PathVariable("postID") UUID postID) {
-        log.info("get postByPostID controller with userID : {}", postID);
-        PostDTO newPostListExample = postService.getPostByPostID(postID);
-        if (newPostListExample == null) {
+        log.info("get postByPostID controller with postID: {}", postID);
+
+        PostDTO getPostByID = postService.getPostByPostID(postID);
+        log.info("get postByPostID: {} successfully", postID);
+        if (getPostByID == null) {
             return new ResponseEntity<>(new ResponseDTO<>(null, "this post not found"), HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(new ResponseDTO<>(newPostListExample, null), HttpStatus.OK);
-
+        return new ResponseEntity<>(new ResponseDTO<>(getPostByID, null), HttpStatus.OK);
     }
 
     @GetMapping(path = "/field/{fieldID}")
     public ResponseEntity<ResponseDTO<List<PostDTO>, String>> getPostByFieldID(
             @PathVariable("fieldID") String fieldID) {
-        log.info("get postByUserID controller with userEmail : {}", fieldID);
+        log.info("get getPostByFieldID controller with fieldID: {}", fieldID);
+
         List<PostDTO> getPostByFieldID = postService.getPostByFieldID(fieldID);
+        log.info("get postByFieldID: {} successfully", fieldID);
         return new ResponseEntity<>(new ResponseDTO<>(getPostByFieldID, null), HttpStatus.OK);
     }
 
     @PostMapping(path = "/")
     public ResponseEntity<ResponseDTO<String, String>> postnewPost(@RequestBody Post newPost) {
+        log.info("post create postcontroller with postName: {} create by userID: {}", newPost.getPostTitle(),
+                newPost.getUserID());
+
         postService.createPost(newPost);
+        log.info("create post by userID: {} successfully withPostID: {} ", newPost.getUserID(), newPost.getPostID());
         return new ResponseEntity<>(new ResponseDTO<>("ok", null), HttpStatus.OK);
     }
 
     @PostMapping(path = "/{postID}/comment")
     public ResponseEntity<ResponseDTO<String, String>> postComment(
             @PathVariable("postID") UUID postID, @RequestBody Comment newComment) {
+        log.info("post create comment controller with by userID: {}", newComment.getUserID());
+
         postService.createComment(postID, newComment);
+        log.info("create comment by userID: {} successfully withPostID: {} ", newComment.getUserID(),
+                newComment.getCommentID());
         return new ResponseEntity<>(new ResponseDTO<>("ok", null), HttpStatus.OK);
     }
 
--- packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostServices/PostService.java
+++ packages/asksenior-post-service/src/main/java/com/jirawat/askseniorpostservice/PostServices/PostService.java
@@ -20,24 +20,32 @@ import com.jirawat.askseniorpostservice.Mapper.PostMapper;
 import com.jirawat.askseniorpostservice.PostRepositories.PostRepository;
 
 import lombok.var;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class PostService {
     @Autowired
     private PostRepository postRepository;
 
     private PostDTO mapPostToPostDTO(Post eachPost) throws JsonMappingException, JsonProcessingException {
-        PostDTO mappedPost = PostMapper.INSTANCE.postToPostDTO(eachPost);
-        CommentJoinPost commentJoinPost;
-        commentJoinPost = JsonHelper.parse(eachPost.getComments(), CommentJoinPost.class);
-        mappedPost.setComments(commentJoinPost.getComments());
+        log.info("call mapPostToPostDTO service by postID: {}", eachPost.getPostID());
 
+        PostDTO mappedPost = PostMapper.INSTANCE.postToPostDTO(eachPost);
+        log.info("mapping post successfully by postID: {}", eachPost.getPostID());
+
+        CommentJoinPost commentJoinPost = JsonHelper.parse(eachPost.getComments(), CommentJoinPost.class);
+        mappedPost.setComments(commentJoinPost.getComments());
+        log.info("parsing comment successfully by postID:: {}", mappedPost.getPostID());
         return mappedPost;
     }
 
     public PostDTO getPostByPostID(UUID postID) {
+        log.info("call getPostByPostID service by postID: {}", postID);
+
         PostDTO output = new PostDTO();
         Post getPost = postRepository.findByPostID(postID);
+        log.info("getPostByPostID successfully by postID: {}", postID);
         if (getPost != null) {
             try {
                 PostDTO mappedPost = mapPostToPostDTO(getPost);
@@ -48,69 +56,82 @@ public class PostService {
                 output.setPostTitle(mappedPost.getPostTitle());
                 output.setUserID(mappedPost.getUserID());
                 output.setUserName(mappedPost.getUserName());
+                log.info("set post output successfully by postID: {}", postID);
             } catch (JsonMappingException e) {
-
+                log.warn("failed to set post output by postID: {} with exeption {}", postID, e);
             } catch (JsonProcessingException e) {
-
+                log.warn("failed to set post output by postID: {} with exeption {}", postID, e);
             }
         } else {
+            log.warn("cannot find this post with postID: {} ", postID);
             return null;
         }
-
         return output;
     }
 
     public List<PostDTO> getPostByFieldID(String fieldID) {
+        log.info("call getPostByFieldID service by fieldID: {}", fieldID);
+
         List<PostDTO> output = new ArrayList<>();
         List<Post> getPost = postRepository.findByFieldID(fieldID);
+        log.info("getPostByfieldID successfully by fieldID: {}", fieldID);
         for (Post eachPost : getPost) {
             try {
                 var postDTO = mapPostToPostDTO(eachPost);
                 output.add(postDTO);
+                log.info("set post output successfully by fieldID: {}", fieldID);
             } catch (JsonMappingException e) {
-
-                e.printStackTrace();
+                log.warn("failed to set post output by fieldID: {} with exeption {}", fieldID, e);
             } catch (JsonProcessingException e) {
-
                 e.printStackTrace();
+                log.warn("failed to set post output by fieldID: {} with exeption {}", fieldID, e);
             }
         }
-
         return output;
     }
 
     public List<PostDTO> getPostByUserID(String userID) {
+        log.info("call getPostByUserID service by userID: {}", userID);
+
         List<PostDTO> output = new ArrayList<>();
+        log.info("getPostByfieldID successfully by userID: {}", userID);
         List<Post> getPost = postRepository.findByUserID(userID);
         for (Post eachPost : getPost) {
             try {
                 var postDTO = mapPostToPostDTO(eachPost);
                 output.add(postDTO);
+                log.info("set post output successfully by userID: {}", userID);
             } catch (JsonMappingException e) {
+                log.warn("failed to set post output by userID: {} with exeption {}", userID, e);
 
-                e.printStackTrace();
             } catch (JsonProcessingException e) {
-
-                e.printStackTrace();
+                log.warn("failed to set post output by userID: {} with exeption {}", userID, e);
             }
         }
-
         return output;
     }
 
     public void createPost(Post post) {
+        log.info("call createPost service ");
+
         UUID uuid = UUID.randomUUID();
         post.setPostID(uuid);
         post.setComments("{\"comments\": []}");
         postRepository.save(post);
+        log.info("createPost successfully by ID: {} ", post.getPostID());
 
     }
 
     public void createComment(UUID postID, Comment comment) {
+        log.info("call createPost service ");
+
         UUID uuid = UUID.randomUUID();
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        log.info("call cmapper and generateUUID");
+
         Post getPost = postRepository.findByPostID(postID);
+        log.info("serchPost by ID: {} ", postID);
         if (postID.equals(getPost.getPostID())) {
             comment.setCommentID(uuid);
             PostDTO mapGetPostToDTO;
@@ -118,16 +139,19 @@ public class PostService {
                 mapGetPostToDTO = mapPostToPostDTO(getPost);
                 List<Comment> getCommentObj = mapGetPostToDTO.getComments();
                 getCommentObj.add(comment);
+                log.info("add comment to post by commentID: {} to postID {} successfully", postID,
+                        comment.getCommentID());
                 String commentToString = mapper.writeValueAsString(getCommentObj);
                 getPost.setComments("{\"comments\":" + " " + commentToString + "}");
                 postRepository.save(getPost);
-
+                log.info("create comment to post with by commentID: {} to postID {} successfully", postID,
+                        comment.getCommentID());
             } catch (JsonMappingException e) {
-
-                e.printStackTrace();
+                log.warn("failed to create comment to post by commentID: {} with exception ",
+                        comment.getCommentID(), e);
             } catch (JsonProcessingException e) {
-
-                e.printStackTrace();
+                log.warn("failed to create comment to post by commentID: {} with exception ",
+                        comment.getCommentID(), e);
             }
 
         }
